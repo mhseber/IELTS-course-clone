248d5e51c0255afaad9393825bfe6bfd911027ce
mahamudul hasan seber <sebermh@gmail.com>
2025-07-26 16:47:58 +0600

    Page SetUp

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+// /** @type {import('tailwindcss').Config} */
+// import daisyui from "daisyui"
+// module.exports = {
+//    content: [
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+//     // Or if using `src` directory:
+//     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//      extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [
+//     daisyui
+//   ],
+//   daisyui: {
+//     themes: ["light"],
+//   },
+// }
+
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+const tailwindConfig = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    daisyui
+  ],
+  daisyui: {
+    themes: ["light"],
+  },
+};
+
+export default tailwindConfig
